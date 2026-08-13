9f42de216803b4342f5f5d99fcb97f44e8c37080
tpopbwi <tpopbanyuwangi@gmail.com>
2026-08-14 06:34:20 +0700

    Update wilayah.js

--- js/wilayah.js
+++ js/wilayah.js
@@ -17,7 +17,7 @@
 // ============================================================
 // 1. KONFIGURASI GLOBAL
 // ============================================================
-const GITHUB_LOGO_URL = "assets/logo.png";
+var GITHUB_LOGO_URL = "assets/logo.png";
 const API_URL = "https://script.google.com/macros/s/AKfycbxfANwhLfJnT1uDqC_4xIFpCvMDLbM0rZcrFPXqLuFc-u0juCrsTgb7v9yGMUedlWiF/exec";
 const placeholderImg = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 85'%3E%3Crect width='60' height='85' fill='%232e446e'/%3E%3Cpath d='M30 40c5.5 0 10-4.5 10-10s-4.5-10-10-10-10 4.5-10 10 4.5 10 10 10zm0 5c-8 0-20 4-20 12v5h40v-5c0-8-12-12-20-12z' fill='%23ffffff' opacity='0.2'/%3E%3C/svg%3E";
 
